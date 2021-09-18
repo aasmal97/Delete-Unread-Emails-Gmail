dbe0dd96cff3c49da1fa19f11ec0063a842c6a54
Arky Asmal <arkyasmal@gmail.com>
2021-09-18 13:16:07 -0400

    "fixed stale element bug"

--- Gmail Scraping/index.py
+++ Gmail Scraping/index.py
@@ -10,35 +10,6 @@ from selenium.webdriver.support import expected_conditions as EC
 
 #to use this, add a cred.py file to project directorywith variable password 
 from cred import password
-
-#setup to avoid bot detection and grab driver (the automated tool)
-os.environ['PATH'] +=  os.pathsep + r"C:/chromeDriver"
-options = webdriver.ChromeOptions()
-options.add_argument("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/93.0.4577.82 Safari/537.36")
-options.add_experimental_option("excludeSwitches", ["enable-automation"])
-options.add_experimental_option('useAutomationExtension', False)
-options.add_argument('--disable-blink-features=AutomationControlled')
-driver = webdriver.Chrome(options=options)
-driver.maximize_window()
-stealth(driver,
-    languages=["en-US", "en"],
-    vendor="Google Inc.",
-    platform="Win32",
-    webgl_vendor="Intel Inc.",
-    renderer="Intel Iris OpenGL Engine",
-    fix_hairline=True,
-    )
-#for randomizing numbers
-seed(1)
-
-#this allows us to naviagte from google to gmail login/or gmail inbox
-driver.get("https://www.google.com/")
-gmailBtn = driver.find_element_by_link_text("Gmail")
-gmailBtn.click()
-
-#Click on Sign in if it exists
-time.sleep(randrange(1,3))
-signInBtn = driver.find_element_by_link_text("Sign in")
 def signIn():
     #user is given 40 sec to manually type in username and click next
     time.sleep(40)
@@ -84,7 +55,35 @@ def nextPage(buttonType, btnPath):
         )
         buttonType.click()
     driver.implicitly_wait(30)
-    buttonType = driver.find_element_by_xpath(btnPath)
+
+#setup to avoid bot detection and grab driver (the automated tool)
+os.environ['PATH'] +=  os.pathsep + r"C:/chromeDriver"
+options = webdriver.ChromeOptions()
+options.add_argument("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/93.0.4577.82 Safari/537.36")
+options.add_experimental_option("excludeSwitches", ["enable-automation"])
+options.add_experimental_option('useAutomationExtension', False)
+options.add_argument('--disable-blink-features=AutomationControlled')
+driver = webdriver.Chrome(options=options)
+driver.maximize_window()
+stealth(driver,
+    languages=["en-US", "en"],
+    vendor="Google Inc.",
+    platform="Win32",
+    webgl_vendor="Intel Inc.",
+    renderer="Intel Iris OpenGL Engine",
+    fix_hairline=True,
+    )
+#for randomizing numbers
+seed(1)
+
+#this allows us to naviagte from google to gmail login/or gmail inbox
+driver.get("https://www.google.com/")
+gmailBtn = driver.find_element_by_link_text("Gmail")
+gmailBtn.click()
+
+#Click on Sign in if it exists
+time.sleep(randrange(1,3))
+signInBtn = driver.find_element_by_link_text("Sign in")
 
 if(signInBtn):
     signInBtn.click()
@@ -100,6 +99,8 @@ while not seeOlderMess.get_attribute("aria-disabled"):
         EC.presence_of_element_located((By.XPATH, "//*[@aria-label='Older']"))
     )
     nextPage(seeOlderMess, "//*[@aria-label='Older']")
+    #avoid stale element error (meaning element position has changed since last assigned)
+    seeOlderMess = driver.find_element_by_xpath("//*[@aria-label='Older']")
 
 #loop back to start
 driver.implicitly_wait(30)
@@ -111,4 +112,5 @@ while not seeRecentMess.get_attribute("aria-disabled"):
         EC.presence_of_element_located((By.XPATH, "//*[@aria-label='Newer']"))
     )
     nextPage(seeRecentMess, "//*[@aria-label='Newer']")
-    
+    #avoid stale element error (meaning element position has changed since last assigned)
+    seeRecentMess = driver.find_element_by_xpath("//*[@aria-label='Newer']")
